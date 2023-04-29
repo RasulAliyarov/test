8468b24dab3f3a6fa61018801159726a54851a0e
RasulAliyarov <rasulaliyarov2002@gmail.com>
2023-04-29 17:12:31 +0400

    without comment

--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <?php
 // Task 1
 
-// $integer = 12345;
-// $rem = 0;
+$integer = 12345;
+$rem = 0;
 
-// while ($integer > 0) {
-//     $temp = (int) $integer % 10;
-//     $integer = (int) $integer / 10;
-//     $rem = $rem + $temp;
-// }
-// echo $rem;
+while ($integer > 0) {
+    $temp = (int) $integer % 10;
+    $integer = (int) $integer / 10;
+    $rem = $rem + $temp;
+}
+echo $rem;
 
 
 // Task 2
-// function ending($number)
-// {
-//     $word = "";
-//     $rem = $number % 10;
+function ending($number)
+{
+    $word = "";
+    $rem = $number % 10;
 
-//     if ($rem == 1) {
-//         $word  = "$number Урок";
-//     } elseif ($rem == 5 || $rem == 6 || $rem == 7 || $rem == 8 || $rem == 9 || $rem == 0) {
-//         $word  = "$number Уроков";
-//     }
-//     else
-//         $word  = "$number Урока";
+    if ($rem == 1) {
+        $word  = "$number Урок";
+    } elseif ($rem == 5 || $rem == 6 || $rem == 7 || $rem == 8 || $rem == 9 || $rem == 0) {
+        $word  = "$number Уроков";
+    }
+    else
+        $word  = "$number Урока";
 
-//     return  $word;
-// }
+    return  $word;
+}
 
-// echo ending(2);
+echo ending(21);
 
 
-// Task 3
+// // Task 3
 function traffic_light()
 {
     date_default_timezone_set('Asia/Baku');
